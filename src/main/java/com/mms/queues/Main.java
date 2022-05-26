8d395755e30bf9220fcfbb8d787dcce1f1b0bbca
DanielBakas <danielbakas@gmail.com>
2022-05-26 17:46:50 -0500

    Update Main

--- src/main/java/com/mms/queues/Main.java
+++ src/main/java/com/mms/queues/Main.java
@@ -17,51 +17,51 @@ package com.mms.queues;
 
 public class Main {
     public static void main(String[] args) {
-        //MM1 | Nosotros 1 ✅
-        //MM1Queue nosotros1 = new MM1Queue(2, 3, 100, 101);
-        //System.out.println(nosotros1.print());
+        //MM1 1 ✅
+        //MM1Queue mm1_1 = new MM1Queue(2, 3, 100, 101);
+        //System.out.println(mm1_1.print());
 
-        //MM1 | Eric 1 ✅
-        //AbstractQueue eric1 = new MM1Queue(161, 162, 101, 120);
-        //System.out.println(eric1.print());
+        //MM1 2 ✅
+        //AbstractQueue mm1_2 = new MM1Queue(161, 162, 101, 120);
+        //System.out.println(mm1_2.print());
 
-        //MM1 | Eric 2 ✅
-        //AbstractQueue eric2 = new MM1Queue(100, 300, 101, 120);
-        //System.out.println(eric2.print());
+        //MM1 3 ✅
+        //AbstractQueue mm1_3 = new MM1Queue(100, 300, 101, 120);
+        //System.out.println(mm1_3.print());
 
-        //MMs | Eric 3 ❌: Stack Overflow in Factorial (too many servers)
-        //AbstractQueue eric3 = new MMsQueue(111, 161, 162, 50, 1);
-        //System.out.println(eric3.print());
+        //MMs 1 ❌: Stack Overflow in Factorial (too many servers)
+        //AbstractQueue mms_1 = new MMsQueue(111, 161, 162, 50, 1);
+        //System.out.println(mms_1.print());
 
-        // MMs | Nosotros 2 ✅
-        //AbstractQueue nosotros2 = new MMsQueue(2, 2, 3, 100, 101);
-        //System.out.println(nosotros2.print());
+        // MMs 2 ✅
+        //AbstractQueue mms_2 = new MMsQueue(2, 2, 3, 100, 101);
+        //System.out.println(mms_2.print());
 
-        // MMsK | Nosotros 3 ✅
-        //AbstractQueue nosotros3 = new MMsKQueue(1, 3, 2, 3, 100, 100);
-        //System.out.println(nosotros3.print());
+        // MMsK 1 ✅
+        //AbstractQueue mmsk_1 = new MMsKQueue(1, 3, 2, 3, 100, 100);
+        //System.out.println(mmsk_1.print());
 
-        //MMs | Eric 4 ❌: Stack Overflow in Factorial (too many servers)
-        //AbstractQueue eric4 = new MMsKQueue(34, 2000000, 12345, 21345, 299999
+        //MMsK 2 ❌: Stack Overflow in Factorial (too many servers)
+        //AbstractQueue mmsk_2 = new MMsKQueue(34, 2000000, 12345, 21345, 299999
         //        , 299999);
-        //System.out.println(eric4.print());
+        //System.out.println(mmsk_2.print());
 
-        // MG1 | Eric 4 ✅
-        //AbstractQueue eric4 = new MG1Queue(Distribution.EXPONENTIAL, 13, 14,
+        // MG1 1 ✅
+        //AbstractQueue mg1_1 = new MG1Queue(Distribution.EXPONENTIAL, 13, 14,
         //        0.99f, 101, 120);
-        //System.out.println(eric4.print());
+        //System.out.println(mg1_1.print());
 
-        // MG1 | Eric 5 ✅
-        //AbstractQueue eric5 = new MG1Queue(Distribution.EXPONENTIAL, 161, 162
+        // MG1 2 ✅
+        //AbstractQueue mg1_2 = new MG1Queue(Distribution.EXPONENTIAL, 161, 162
         //        , 0, 101, 120);
-        //System.out.println(eric5.print());
+        //System.out.println(mg1_2.print());
 
-        // MEks | Eric 6 ✅
-        //AbstractQueue eric6 = new MEksQueue(1, 2, 3, 2, 2);
-        //System.out.println(eric6.print());
+        // MEks 1 ✅
+        //AbstractQueue meks_1 = new MEksQueue(1, 2, 3, 2, 2);
+        //System.out.println(meks_1.print());
 
-        // MEks | Eric 7 ✅
-        //AbstractQueue eric7 = new MEksQueue(5, 25, 30, 1, 1);
-        //System.out.println(eric7.print());
+        // MEks 2 ✅
+        //AbstractQueue meks_2 = new MEksQueue(5, 25, 30, 1, 1);
+        //System.out.println(meks_2.print());
     }
 }
