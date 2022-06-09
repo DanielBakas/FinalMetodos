04c445cb6cb72f57c8de27b1b674da115db5bdbd
Mauville <4991127@outlook.com>
2022-06-08 19:59:24 -0500

    Readd integration testing

--- src/test/java/com/mms/api/BackendApplicationTests.java
+++ src/test/java/com/mms/api/BackendApplicationTests.java
@@ -1,12 +1,48 @@
-package com.mms.api;
+package com.mms.backend;
 
+import com.mms.api.BackendApplication;
+import com.mms.api.QueueController;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private BackendApplication backendApplication;
+
+    private TestRestTemplate testRestTemplate;
+
+
+    void demo() {
+        JSONObject request = new JSONObject();
+//        request.put("Distribution", "EXPONENTIAL");
+        request.put("s", "1");
+        request.put("k", "3");
+        request.put("lambda", "2");
+        request.put("mu", "3");
+        request.put("cw", "100");
+        request.put("cs", "100");
+
+        HttpEntity<String> httprequest = new HttpEntity<String>(request.toString());
+        ResponseEntity<String> responsed = this.testRestTemplate.postForEntity("http://localhost:" + port + "/mmsk", httprequest, String.class);
+        String response =  responsed.toString();
+        assertTrue(response.contains("0.666"));
+        assertTrue(response.contains("0.4308"));
+        assertTrue(response.contains("1.0154"));
+        assertTrue(response.contains("0.2456"));
+        assertTrue(response.contains("0.5789"));
+        assertTrue(response.contains("j143.0769"));
     }
 }
+
